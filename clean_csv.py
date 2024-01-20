ddc3a2888269d12ee3d442ce4e6b07f29f8040dd
Peter M Grund <petermgrund@gmail.com>
2024-01-20 00:43:34 -0600

    Refactor CSV cleaning and conversion scripts

--- clean_csv.py
+++ clean_csv.py
@@ -6,12 +6,8 @@ directory = '../dysarthria_analysis/ddk_data/'
 
 for filename in os.listdir(directory):
     if filename.endswith(".csv"):
-        # Rename the file to remove the first underscore
-        new_filename = filename.replace("_", "", 1)
-        os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
-
         # Read the file into a pandas DataFrame
-        df = pd.read_csv(os.path.join(directory, new_filename))
+        df = pd.read_csv(os.path.join(directory, filename))
 
         df = df.rename(columns={
             'xmin': 'time_start',
@@ -29,25 +25,27 @@ for filename in os.listdir(directory):
 
         df["letter"] = df["letter"].str.replace(" ", "")
 
-        # Update "study_ID" and "brain_side" based on the first 6 characters of the file name
-        study_ID = new_filename[:6]
+        # Update "study_ID" and "brain_side" based on the characters of the file name
+        study_ID = filename.split('_')[0]
         df["study_ID"] = study_ID
-        df["brain_side"] = study_ID[-1]
+
+        brain_side = filename.split('_')[1]
+        df["brain_side"] = brain_side
 
         # Update "visit" based on the presence of certain keywords in the file name
         for keyword in ["Baseline", "6mo", "12mo", "18mo", "24mo"]:
-            if keyword in new_filename:
+            if keyword in filename:
                 df["visit"] = keyword
                 break
 
         # Update "mA" based on the presence of a pattern in the file name
-        mA_match = re.search(r'(\d+\.\d+)mA', new_filename)
+        mA_match = re.search(r'(\d+\.\d+)mA', filename)
         if mA_match:
             df["mA"] = mA_match.group(1)
         
-        ordinality_match = re.search(r'(Baseline|6mo|12mo|18mo|24mo)_(\d{2})', new_filename)
+        ordinality_match = re.search(r'(Baseline|6mo|12mo|18mo|24mo)_(\d{2})', filename)
         if ordinality_match:
             df["ordinality"] = ordinality_match.group(2)
 
         # Write the DataFrame back to the CSV file
-        df.to_csv(os.path.join(directory, new_filename), index=False)
+        df.to_csv(os.path.join(directory, filename), index=False)
