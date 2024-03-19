d27dfcca0dee99a20e7a33e29c0b2dd73306a6f8
Peter M Grund <petermgrund@gmail.com>
2024-03-19 14:00:28 -0500

    consolidated 2 scripts into 1

--- convert_underscore.py
+++ convert_underscore.py
@@ -8,4 +8,11 @@ def rename_files(directory):
             os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
             print(f"Renamed {filename} to {new_filename}")
 
-rename_files('data/raw/')
+def rename_files_cleanup(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".wav") and "_00" in filename:
+            new_filename = filename.replace("_00", ".00")
+            os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+
+rename_files('data/raw')
+rename_files_cleanup('data/raw/')
