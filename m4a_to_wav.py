e0535ae7f766501c7bead93271e01cb4cf07bcec
Peter M Grund <petermgrund@gmail.com>
2024-05-04 17:07:15 -0500

    Add m4a to wav conversion script

---
+++ m4a_to_wav.py
@@ -0,0 +1,78 @@
+from pydub import AudioSegment
+import os
+import shutil
+
+def get_m4a_files(directory):
+    return [f for f in os.listdir(directory) if f.endswith(".m4a")]
+
+def get_textgrid_files(directory):
+    return [f for f in os.listdir(directory) if f.endswith('.TextGrid')]
+
+def copy_textgrid_files(input_directory, output_directory):
+    count = 0
+    for file in os.listdir(input_directory):
+        if file.endswith(".TextGrid"):
+            shutil.copy(os.path.join(input_directory, file), output_directory)
+            count += 1
+    return count
+
+def convert_all_m4a_to_wav(input_directory, output_directory):
+    all_conversions_successful = True
+    for filename in get_m4a_files(input_directory):
+        m4a_path = os.path.join(input_directory, filename)
+        wav_path = os.path.join(output_directory, os.path.splitext(filename)[0] + ".wav")
+        print(f"Converting {filename} to {wav_path}...")
+        try:
+            audio = AudioSegment.from_file(m4a_path, format="m4a")
+            audio.export(wav_path, format="wav")
+            print(f"Successfully converted {filename} to .wav format")
+        except Exception as e:
+            print(f"Failed to convert {m4a_path} to {wav_path}. Error: {e}")
+            all_conversions_successful = False
+    return all_conversions_successful
+default_directory = "../../Patients/"
+print("Please enter the root directory containing m4a files to convert. Press Enter to use the default directory.")
+
+try:
+    root_directory = input(f"\033[0;30mRoot directory (default: {default_directory}): \033[0m") or default_directory
+except KeyboardInterrupt:
+    print("\n\033[0;31mScript interrupted by user.\033[0m")
+    exit(1)
+    
+print("Please enter a subdirectory or multiple subdirectories (separated by commas) of \033[94m" + default_directory + "\033[0m containing m4a files to convert. Example: CRP-01/D_Visit_1/Audio_recordings,CRP-01/D_Visit_2/Audio_recordings")
+try:
+    subdir_input = input("\033[0;30mSubdirectory: \033[0m")
+    if ',' in subdir_input:
+        subdirs = subdir_input.split(',')
+    else:
+        subdirs = [subdir_input]
+except KeyboardInterrupt:
+    print("\n\033[0;31mScript interrupted by user.\033[0m")
+    exit(1)
+
+for subdir in subdirs:
+    subdir = subdir.strip()
+    input_directory = os.path.join(root_directory, subdir)
+
+    etc_dir = subdir.split('/')[0]
+    if not etc_dir.startswith('CRP-'):
+        print("\033[0;31mCould not extract CRP-XX from subdirectory. Please check your input.\033[0m")
+        continue  # Skip to the next directory
+    output_directory = "data/raw"
+
+    try:
+        m4a_files = get_m4a_files(input_directory)
+        textgrid_files = get_textgrid_files(input_directory)
+
+        all_conv_success = convert_all_m4a_to_wav(input_directory, output_directory)
+    except FileNotFoundError:
+        print("\033[0;31mThe specified directory does not exist.\033[0m")
+        continue  # Skip to the next directory
+
+    if all_conv_success:
+        print("\033[0;32mAll conversions are complete and have been saved to DDKTor.\033[0m")
+    else:
+        print("\033[0;31mAt least one conversion failed. Make sure your input and output directories exist.\033[0m")
+
+    textgrid_count = copy_textgrid_files(input_directory, output_directory)
+    print(f"\033[0;33m{textgrid_count} TextGrid files have also been transferred to DDKTor.\033[0m")
